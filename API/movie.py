930a4ab81a0d8701dbf301de7b4d94e591ad2536
Nipun Kumar Ranjan <nkr.062005@gmail.com>
2026-05-01 20:54:29 +0530

    API for viewing JSON movie file

---
+++ API/movie.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+import json
+
+def load_data():
+    with open('genre_mapping.json',"r") as f:
+        data=json.load(f)
+        print(data)
+    return data
+
+app=FastAPI()
+
+@app.get("/")
+def intro():
+    return {"Welcome to Movie View System"}
+
+@app.get("/view")
+def view():
+    data=load_data()
+    return data
