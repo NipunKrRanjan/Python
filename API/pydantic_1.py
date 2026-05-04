65ed4c8503fab47a080f604f0fbc215f41b71058
Nipun Kumar Ranjan <nkr.062005@gmail.com>
2026-05-04 22:58:14 +0530

    Adding Pydantic Data Types

--- API/pydantic_1.py
+++ API/pydantic_1.py
@@ -1,17 +1,22 @@
-from pydantic import BaseModel
+from pydantic import BaseModel,EmailStr
+from typing import Optional
 
 class Patient(BaseModel):
     name:str
     age:int
+    email:EmailStr
+    allergies:Optional[str]=None
 
 Patient_info={
     'name':'Nipun',
-    'age':30
+    'age':30,
+    'email':'abc@gmail.com'
 }
 patient1=Patient(**Patient_info)
 
 def insert(patient:Patient):
     print(patient.name)
     print(patient.age)
+    print(patient.email)
 
 insert(patient1)
