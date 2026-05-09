91b08c61f8d4d425ab3a27c1069bf3712a569146
Nipun Kumar Ranjan <nkr.062005@gmail.com>
2026-05-09 13:30:49 +0530

    Updating patient data and deleting patient data

--- API/update_data.py
+++ API/update_data.py
@@ -1,8 +1,9 @@
 from pydantic import BaseModel,field_validator,computed_field,Field
 from fastapi import FastAPI,HTTPException,Path
+from fastapi.responses import JSONResponse
 from typing import Optional,List,Dict,Annotated,Literal
 import os
-import json
+import json 
 
 app=FastAPI()
 filename='patient_data.json'
@@ -25,7 +26,7 @@ class Patient(BaseModel):
     address:Annotated[Address,Field(...,description="Enter the address of patient")]
     height:Annotated[float,Field(...,gt=0,description="Enter the height of patient in mteres")]
     weight:Annotated[float,Field(...,gt=0,description="Enter the weight of patient in kgs")]
-    allergies:Optional[Annotated[List,Field(description="Enter allregies if any")]]
+    allergies:Annotated[Optional[List[str]], Field(default=[], description="Enter allergies if any")]
 
     @computed_field
     @property
@@ -102,11 +103,11 @@ def add_data(patient:Patient):
 
 #Updating data based on Pid
 @app.put('/update_data/{pid}')
-def update_data(patient_id:str,updated_info:update_data):
+def update_data(updated_info:update_data,pid:str=Path(example="P001")):
     data=load_data()
-    if(data=={} or patient_id not in data):
+    if(data=={} or pid not in data):
         raise HTTPException(status_code=404,detail="Patient does not exist")
-    existing_info=data[patient_id]
+    existing_info=data[pid]
 
     updated_data=updated_info.model_dump(exclude_unset=True)
 
@@ -114,9 +115,20 @@ def update_data(patient_id:str,updated_info:update_data):
         existing_info[key]=value
 
     #Now we need to recalculate the values of computed fields, so we follow the logic existing_info -> pydantic object of class Patient -> Calculation of computed fields->back to dictionary 
-    existing_info['pid']=patient_id
+    existing_info['pid']=pid
     existing_info_object=Patient(**existing_info)
-    existing_info=existing_info_object.model_dump(exclude='pid')
-    data[patient_id]=existing_info
+    existing_info=existing_info_object.model_dump(exclude={'pid'})
+    data[pid]=existing_info
     save_data(data=data)
+    return JSONResponse(status_code=200,content={"Message":"Data Updated Successfully"})
     
+@app.delete("/delete_data/{pid}")
+def delete_user(pid:str=Path(example="P001")):
+    data=load_data()
+    if(pid not in data):
+        raise HTTPException(status_code=404,detail="Patient does not exist")
+    if(data=={}):
+        raise HTTPException(status_code=404, detail="No data available")
+    del data[pid]
+    save_data(data)
+    return {"Message":"Data Updated Successfully"}
